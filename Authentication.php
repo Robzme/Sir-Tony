9d7394595ba518f3e960e99334071fbe10033559
Robzme <akosirobul@gmail.com>
2026-03-24 09:07:02 +0800

    Fix formatting and remove unnecessary whitespace

--- Authentication.php
+++ Authentication.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once "conn.php"
 // Encryption helpers
 function _uylogres_encryption_key(): string {
     return hash('sha256', 'uylogres_secret_key_2026', true);
@@ -63,7 +63,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["register"])){
     }
 };
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -119,3 +118,4 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["register"])){
     </script>
 </body>
 </html>
+
